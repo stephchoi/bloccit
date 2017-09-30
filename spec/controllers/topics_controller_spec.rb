ef259d523cd29fe4c7d41c117ae361445c9835b2
Stephany Choi <s.choi417@gmail.com>
2017-09-30 16:42:40 +0000

    Add new and create methods

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -32,4 +32,36 @@ RSpec.describe TopicsController, type: :controller do
         end
    end
    
+    describe "GET new" do
+        it "returns http success" do
+            get :new
+            expect(response).to have_http_status(:success)
+        end
+ 
+        it "renders the #new view" do
+            get :new
+            expect(response).to render_template :new
+        end
+ 
+        it "initializes @topic" do
+            get :new
+            expect(assigns(:topic)).not_to be_nil
+        end
+   end
+   
+    describe "POST create" do
+        it "increases the number of topics by 1" do
+            expect{ post :create, params: {topic: {name: RandomData.random_sentence, description: RandomData.random_paragraph}}}.to change(Topic,:count).by(1)
+        end
+   
+        it "assigns Topic.last to @topic" do
+            post :create, params: {topic: {name: RandomData.random_sentence, description: RandomData.random_paragraph}}
+            expect(assigns(:topic)).to eq Topic.last
+        end
+ 
+        it "redirects to the new topic" do
+            post :create, params: {topic: {name: RandomData.random_sentence, description: RandomData.random_paragraph}}
+            expect(response).to redirect_to Topic.last
+        end
+   end
 end
