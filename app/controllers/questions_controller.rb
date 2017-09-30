4375f0ac2716fab366bb6d5c727caee39e22aed1
Stephany Choi <s.choi417@gmail.com>
2017-09-30 03:30:12 +0000

    Finish assignment-19

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,6 +13,7 @@ class QuestionsController < ApplicationController
 
   def create
     @question = Question.new
+    
     @question.title = params[:question][:title]
     @question.body = params[:question][:body]
     @question.resolved = params[:question][:resolved]
