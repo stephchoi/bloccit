8575897f2c31429bbe216c8cc9ed5cb74fae5712
Stephany Choi <s.choi417@gmail.com>
2017-10-01 22:24:18 +0000

    Change sponsered views and topic show

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,8 +26,19 @@ posts = Post.all
         body: RandomData.random_paragraph
    )
 end
+
+# Create Sponsered Posts
+30.times do
+    SponseredPost.create!(
+        topic: topics.sample,
+        title: RandomData.random_sentence, 
+        body: RandomData.random_paragraph, 
+        price: rand(5..100)
+    )
+end
  
 puts "Seed finished"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{SponseredPost.count} posts created"
