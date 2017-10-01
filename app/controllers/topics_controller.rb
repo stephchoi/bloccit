128b6142685f483b8ae9e160c645a2048b696eac
Stephany Choi <s.choi417@gmail.com>
2017-10-01 00:39:05 +0000

    Add delete, edit and update methods and specs

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -24,4 +24,34 @@ class TopicsController < ApplicationController
             render :new
         end
     end
+    
+    def edit
+       @topic = Topic.find(params[:id]) 
+    end
+    
+    def update
+        @topic = Topic.find(params[:id])
+        @topic.name = params[:topic][:name]
+        @topic.description = params[:topic][:description]
+        @topic.public = params[:topic][:public]
+        
+        if @topic.save
+            redirect_to @topic, notice: "Topic was updated."
+        else
+            flash.now[:alert] = "Error saving topic. Please try again."
+            render :edit
+        end
+    end
+    
+    def destroy
+       @topic = Topic.find(params[:id])
+       
+       if @topic.destroy
+           flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
+           redirect_to action: :index
+       else
+           flash.now[:alert] = "There was an error deleting the topic."
+           render :show
+       end
+    end
 end
