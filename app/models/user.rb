f7964ad5ea291fcbffbe4d523d3fb3b621196606
Stephany Choi <s.choi417@gmail.com>
2017-10-02 19:50:41 +0000

    Add User name format method and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
    before_save { self.email = email.downcase if email.present? }
+   before_save :format_name
 
    validates :name, length: { minimum: 1, maximum: 100 }, presence: true
    validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
@@ -10,4 +11,12 @@ class User < ApplicationRecord
              length: { minimum: 3, maximum: 254 }
 
    has_secure_password
+   
+   def format_name
+       if name 
+           name_array = []
+           name.split.each { |w| name_array << w.capitalize! } 
+           self.name = name_array.join(" ")
+       end
+   end
 end
