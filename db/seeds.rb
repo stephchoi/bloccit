22a66c1d6a61b16f55286e284c3b1b1c14d13d64
Stephany Choi <s.choi417@gmail.com>
2017-10-04 16:46:15 +0000

    REfactor post controller with roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,10 +38,17 @@ posts = Post.all
    )
 end
 
-user = User.first
-user.update_attributes!(
-    email: 's.choi417@gmail.com', 
-    password: 'helloworld'
+admin = User.create!(
+    name:       'Admin User', 
+    email:      'admin@example.com', 
+    password:   'helloworld', 
+    role:       'admin'
+)
+
+member =  User.create!(
+    name:       'Member User', 
+    email:      'member@example.com', 
+    password:   'helloworld'
 )
  
 puts "Seed finished"
