3c03f8cf2bfce55b761238f969b0f25b088e63ac
Stephany Choi <s.choi417@gmail.com>
2017-10-06 14:16:07 +0000

    Create favorite mailer controller and views

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,5 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   
   config.action_mailer.default_url_options = { host: 'localhost' }
+  config.raise_delivery_errors = true
 end
