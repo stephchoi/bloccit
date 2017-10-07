cce10b9830cbd189ad86a74dea6a311de3580507
Stephany Choi <s.choi417@gmail.com>
2017-10-07 15:51:43 +0000

    Add method for detecting user posts/comments

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+    def user_has_no_posts?
+        current_user.posts.count == 0 
+    end
+    
+    def user_has_no_comments?
+       current_user.comments.count == 0 
+    end
 end
